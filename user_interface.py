f026c6d36d54110b1843e51565855da7dc543cc5
NikolayM <malinin2123@gmail.com>
2025-02-26 21:50:53 +0300

    В user_interface.py добавил возможность настройки скорости обмена данных (baud rate); добавил возможность считывать поля, отмеченные галочкой, которые соответствуют устройствам R1-R16, T1-T9; добавил дополнительные возможности: 1. При нажатии на кнопку 'получить' (введенные данные) в консоль выводятся все настройки которые были введены пользователем на момент нажатия. 2. При нажатии на кнопку 'сохранить' (сохранение данных) происходит сохранение введенных настроек в выбранную директорию на компьютере пользователя в виде txt-файла; подгрузил в интерфейс пустой график, используя библиотеку matplotlib; добавил к графику панель управления из класса NavigationToolbar2Tk(). Столкнулся с проблемой инициализации переменных port и devices_connected, которую пока не решил. Frame 'Данные с сенсоров и термопар' временно закомментирован для упрощения

--- user_interface.py
+++ user_interface.py
@@ -1,13 +1,18 @@
 from tkinter import *
 import tkinter as tk
 from tkinter import filedialog
-from tkinter.ttk import Checkbutton
+from tkinter.ttk import Checkbutton, Combobox
 from tkinter.messagebox import showerror, showwarning, showinfo
 from functools import partial
 import serial
 import serial.tools.list_ports
+import matplotlib.pyplot as plt
+import matplotlib.figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+import numpy as np
 
 class App(tk.Tk):
+
     def is_it_float(self, newval):
         valid_symbols = list("-,.0123456789")
         for symb in newval:
@@ -22,58 +27,90 @@ class App(tk.Tk):
                 return False
         return True
 
-    def selected_COM_port(self, List, connected):
+    def COM_port_setting(self, List, connected, combo):
         select = list(List.curselection())
         if len(select) != 0:
             global port
             port = connected[int(select[0])]
+            #used_port = port
             print("Успешно выбран порт", port)
         else:
             showerror(title="Ошибка", message="Порт не выбран!")
             print("Порт не выбран")
+        print("Baud rate:", combo.get())
     def coefficients_info(self, Kp, Ki, Kd, default_Kp, default_Ki, default_Kd):
         if (Kp.get() == 0 or Ki.get() == 0 or Kd.get() == 0):
             showinfo(title="Внимание",
                      message="Некоторые коэффициенты ПИД-регулятора не заданы, при работе будут использованы значения, заданные по умалчанию")
-        print("Введенные коэффициенты: entry_Kp =", Kp.get(), "; entry_Ki =", Ki.get(), "; entry_Kd =", Kd.get())
+        print("Введенные коэффициенты: Kp =", Kp.get(), "; Ki =", Ki.get(), "; Kd =", Kd.get())
         if (Kp.get() == 0): Kp.set(default_Kp)
         if (Ki.get() == 0): Ki.set(default_Ki)
         if (Kd.get() == 0): Kd.set(default_Kd)
-        print("Коэффициенты для ПИД-регулятора: entry_Kp =", Kp.get(), "; entry_Ki =", Ki.get(), "; entry_Kd =", Kd.get())
-
+        print("Коэффициенты для ПИД-регулятора: Kp =", Kp.get(), "; Ki =", Ki.get(), "; Kd =", Kd.get())
     def manual_mode_info(self, entry_T_max, entry_ΔT, entry_Δt, entry_averaging, entry_Δt_R_T):
-        print("entry_T_max =", entry_T_max.get(), "; entry_ΔT =", entry_ΔT.get(), "; entry_Δt =", entry_Δt.get(),
+        print("T_max =", entry_T_max.get(), "; ΔT =", entry_ΔT.get(), "; Δt =", entry_Δt.get(),
               "; <...>n =", entry_averaging.get(), "; Δt_R&T =", entry_Δt_R_T.get())
     def choose_mode_file(self,  entry_file_address):
         file_path = filedialog.askopenfilename()
         entry_file_address.set(file_path)
         print("Был выбран файл:", file_path)
+    def devices_used(self, chk_state_R, chk_state_T):
+        global devices_connected
+        devices_connected = []
+        for i in range(1, 17):
+            if (chk_state_R[i].get() == 1):
+                devices_connected.append("R%i"%i)
+        for i in range(1, 10):
+            if (chk_state_T[i].get() == 1):
+                devices_connected.append("T%i"%i)
+        print("Подключенные устройства:", devices_connected)
     def run_mode_file(self,  entry_file_address, line_counter):
         file_path = entry_file_address.get()
         f = open(file_path, 'r', encoding='cp1252')
         command_line = ''
         value = line_counter.get()
-       # line_counter = 0
+        value = 0
         for line in f:
             command_line += line.strip() + ' '
-            #line_counter+=1
             value+=1
         f.close()
         line_counter.set(value)
         print("Данные из файла", file_path, ": ", command_line, "\nКоличество команд:", line_counter.get())
     def stop_mode_file(self):
         print("Функция пока не готова")
-    def input_data(selfport, port, Kp, Ki, Kd, entry_file_address):
-        print("Выбранный порт:", port, "; Коэффициенты ПИД:", Kp.get(), Ki.get(), Kd.get(),
-              "Режим работы (сценарий-0/ручной-1):", "Адрес файла-сценария:", entry_file_address.get())
-    def save_data(self):
-        print("Функция пока не готова")
+    def input_data(self, combo, Kp, Ki, Kd, entry_file_address): #port, , devices_connected
+        try:
+            print("Выбранный порт:", port, "; Baud rate:", combo.get(), "; Подключенные устройства:", devices_connected,
+                  ";\nКоэффициенты ПИД:", Kp.get(), Ki.get(), Kd.get(), "; Режим работы (сценарий-0/ручной-1):",
+                  ";\nАдрес файла-сценария:", entry_file_address.get())
+        except NameError:
+            port = "нету его"
+            devices_connected = ["неа"]
+            print("Выбранный порт:", port, "; Baud rate:", combo.get(), "; Подключенные устройства:", devices_connected,
+                  ";\nКоэффициенты ПИД:", Kp.get(), Ki.get(), Kd.get(), "; Режим работы (сценарий-0/ручной-1):",
+                  ";\nАдрес файла-сценария:", entry_file_address.get())
+    def save_data(self, combo, Kp, Ki, Kd, entry_file_address):
+        devices_connected = []
+        port= ''
+        if (len(devices_connected) == 0):
+            showinfo(title="Внимание", message="Не выбраны подключенные устройства! Запустить измерение не получится.")
+        global saved_file_path
+        saved_file_path = filedialog.asksaveasfilename(initialfile="Untitled.txt", filetypes=[("Text files", "*.txt")])
+        if saved_file_path:
+            f = open(saved_file_path, 'w+')
+            f.writelines(f'Выбранный порт: {port}; Baud rate: {combo.get()}; Подключенные устройства: {devices_connected}\n'
+                         f'Коэффициенты ПИД: {Kp.get()} {Ki.get()} {Kd.get()}; Режим работы (сценарий-0/ручной-1): ;\n'
+                         f'Адрес файла-сценария: {entry_file_address.get()}')
+            #print(f"Saving preferences to {saved_file_path}")
+            f.close()
+        else:
+            print("File save operation cancelled.")
 
     def __init__(self):
         super().__init__()
 
         check = (self.register(self.is_it_float), "%P")
-
+        #devices_connected = []
         opts = {'padx': 5, 'pady': 5, 'sticky': 'nswe'}
         #common_entry_ui_params = {'padx': 5, 'pady': 5, 'sticky': 'nswe', 'validate': key, 'validatecommand': check}
 
@@ -82,17 +119,22 @@ class App(tk.Tk):
         for element in port_list:
             connected.append(element.device)
         print("connected COM ports: " + str(connected))
-        port = ''
+        #port = None
 
         group_1_t = tk.LabelFrame(self, padx=15, pady=10, text="COM-порт")
         group_1_t.grid(padx=10, pady=5, row=1, column=1, sticky=tk.NSEW)
-        tk.Label(group_1_t, text="COM-порт:").grid(row=1)
+        tk.Label(group_1_t, text="COM-порт:").grid(row=1, column=1)
         List = tk.Listbox(group_1_t, width=20, height=2, selectmode='SINGLE')
         for i in range(len(connected)):
             List.insert(tk.END, connected[i])
         List.grid(row=1, column=2, sticky=tk.W)
-        tk.Button(group_1_t, text="Подтвердить", bg="yellow", command=partial(self.selected_COM_port, List, connected)).grid(row=2, column=1, columnspan=2)
-        # print(port)
+        tk.Label(group_1_t, text="Скорость обмена\n данных по порту:").grid(row=2, column=1)
+        combo = Combobox(group_1_t, width=15)
+        combo['values'] = (4800, 9600, 19200, 38400)
+        combo.current(1)
+        combo.grid(row=2, column=2)
+        tk.Button(group_1_t, text="Подтвердить", bg="yellow",
+                  command=partial(self.COM_port_setting, List, connected, combo)).grid(row=3, column=1, columnspan=2)
 
         default_Kp, default_Ki, default_Kd = 3, 2, 1
         Kp, Ki, Kd = DoubleVar(value=default_Kp), DoubleVar(value=default_Ki), DoubleVar(value=default_Kd)
@@ -146,6 +188,14 @@ class App(tk.Tk):
         # <...>n -- к-во съемок\n для усреднения
         # Δt_R&T -- периодичность\n съемки R и T
 
+        group_5_t = tk.LabelFrame(self, padx=15, pady=10, text="Дополнительные возможности")
+        group_5_t.grid(padx=10, pady=5, row=5, column=1, sticky=tk.NSEW)
+        tk.Label(group_5_t, text="Введенные данные:").grid(row=1, column=1)
+        tk.Button(group_5_t, text="Получить",
+                  command=partial(self.input_data, combo, Kp, Ki, Kd, entry_file_address)).grid(row=1, column=2) #port,, devices_connected
+        tk.Label(group_5_t, text="Сохранение данных:").grid(row=2, column=1)
+        tk.Button(group_5_t, text="Сохранить", command=partial(self.save_data, combo, Kp, Ki, Kd, entry_file_address)).grid(row=2, column=2)
+
         group_1_r = tk.LabelFrame(self, padx=15, pady=10, text="Подключенные сенсоры и термопары")
         group_1_r.grid(padx=10, pady=5, row=1, column=2, sticky=tk.NSEW)
         chk_state_R = dict()
@@ -155,76 +205,36 @@ class App(tk.Tk):
             chk_state_R[i] = BooleanVar()
             chk_state_R[i].set(False)
             chk_R[i] = Checkbutton(group_1_r, var=chk_state_R[i]).grid(row=2, column=i)
-            chk_state_R = dict()
-            chk_R = dict()
-            for i in range(1, 17):
-                Label(group_1_r, text="R%i" % i).grid(row=1, column=i)
-                chk_state_R[i] = BooleanVar()
-                chk_state_R[i].set(False)
-                chk_R[i] = Checkbutton(group_1_r, var=chk_state_R[i]).grid(row=2, column=i)
-            chk_state_T = dict()
-            chk_T = dict()
-            for i in range(1, 10):
-                Label(group_1_r, text="T%i" % i).grid(row=3, column=i)
-                chk_state_T[i] = BooleanVar()
-                chk_state_T[i].set(False)
-                chk_T[i] = Checkbutton(group_1_r, var=chk_state_T[i]).grid(row=4, column=i)
-            tk.Button(group_1_r, text="Подтвердить", bg="yellow").grid(row=3, column=13, rowspan=2, columnspan=4)
+        chk_state_T = dict()
+        chk_T = dict()
+        for i in range(1, 10):
+            Label(group_1_r, text="T%i" % i).grid(row=3, column=i)
+            chk_state_T[i] = BooleanVar(value=False)
+            chk_T[i] = Checkbutton(group_1_r, var=chk_state_T[i]).grid(row=4, column=i)
+        tk.Button(group_1_r, text="Подтвердить", bg="yellow", command=partial(self.devices_used, chk_state_R, chk_state_T)).grid(row=3, column=13, rowspan=2, columnspan=4)
 
         group_2_r = tk.LabelFrame(self, padx=15, pady=10, text="График R(T)")
-        group_2_r.grid(padx=10, pady=5, row=2, column=2, sticky=tk.NSEW)
-        tk.Label(group_2_r, text="Пока пусто").grid(row=0)
+        group_2_r.grid(padx=10, pady=5, row=2, column=2, rowspan=3, sticky=tk.NSEW)
+        #tk.Label(group_2_r, text="Пока пусто").grid(row=0)
+        fig = matplotlib.figure.Figure(figsize=(4,3), dpi=100)
+        ax = fig.add_subplot(1,1,1)
+        canvas = FigureCanvasTkAgg(fig, master=group_2_r)
+        canvas.get_tk_widget().grid(row=1)
+        toolbar = NavigationToolbar2Tk(canvas, group_2_r, pack_toolbar=False)
+        toolbar.update()
+        toolbar.grid(row=2, sticky=tk.W)
 
+        '''
         group_3_r = tk.LabelFrame(self, padx=15, pady=10, text="Данные с сенсоров и термопар")
-        group_3_r.grid(padx=10, pady=5, row=3, column=2, sticky=tk.NSEW)
+        group_3_r.grid(padx=10, pady=5, row=5, column=2, sticky=tk.NSEW)
         tk.Label(group_3_r, text="R1").grid(row=0, column=1)
         tk.Label(group_3_r, text="R2").grid(row=0, column=3)
         tk.Label(group_3_r, text="R3").grid(row=2, column=1)
         tk.Label(group_3_r, text="R4").grid(row=2, column=3)
         tk.Label(group_3_r, text="T1").grid(row=1, column=2)
-
-        group_4_r = tk.LabelFrame(self, padx=15, pady=10, text="Дополнительные возможности")
-        group_4_r.grid(padx=10, pady=5, row=4, column=2, sticky=tk.NSEW)
-        tk.Label(group_4_r, text="Введенные данные:").grid(row=1, column=1)
-        tk.Button(group_4_r, text="Получить",
-                  command=partial(self.input_data, port, Kp, Ki, Kd, entry_file_address)).grid(row=1, column=2)
-        tk.Label(group_4_r, text="Сохранение данных:").grid(row=1, column=1)
-        tk.Button(group_4_r, text="Сохранить", command=self.save_data).grid(row=1, column=2)
-
+        '''
 
 if __name__ == "__main__":
     app = App()
     app.title("Интерфейс пользователя печки")
     app.mainloop()
-
-
-'''
-from tkinter import *
-from tkinter import ttk
-
-# настройка окна
-root = Tk()
-root.title("Интерфейс пользователя печки")
-root.geometry("800x600")
-# root.config(background = "Light Grey")
-
-#Изображение меток, книпок
-common_label_ui_params = {"font": ('Comic Sans MS', 12, 'bold'), "bd": 10}
-common_grid_ui_params = dict(padx=30, pady=5)
-
-Label_quit = Label(root, text = 'Quit', **common_label_ui_params)
-Label_quit.pack()
-
-root.mainloop()
-
-'''
-
-'''
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-window = tkinter.Tk()
-
-Label_quit = Label(root, text = "quit", bg = "white", fg = "black", font = ("Arial", 30))
-'''
